0787026734ac10fbd8933e1c120a4f94b54d04ea
Brian Birdwell <bbirdwell@gmail.com>
2018-10-26 03:08:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,17 @@
 class Student
+    attr_accessor :name, :grade
+    attr_reader   :id
 
   def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
   end
   
-  def save
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER);
+    SQL
+  end
     
   
 end
